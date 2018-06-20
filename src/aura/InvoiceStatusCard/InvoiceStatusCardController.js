072168a93530f6d3af4dc143c9f073f59bae4286
tibeal <tibeal@gmail.com>
2018-06-20 14:15:48 -0300

    Card Issue

--- src/aura/InvoiceStatusCard/InvoiceStatusCardController.js
+++ src/aura/InvoiceStatusCard/InvoiceStatusCardController.js
@@ -3,7 +3,9 @@
 		var record = component.get('v.record');
         var output = $A.get('$Label.c.YourInvoiceIs') + ' ' + record.FieloPRP__Status__label.toLowerCase();
         component.set('v.output',output);
-        component.set('v.outputSubtitle', record.FieloPRP__Comments__c);
+        if (record.FieloPRP__Status__c == 'Rejected' || record.FieloPRP__Status__c == 'Approved') {
+        	component.set('v.outputSubtitle', record.FieloPRP__Comments__c);    
+        }
         component.set('v.iconName', helper.iconMap[record.FieloPRP__Status__c]);
         component.set('v.statusClass', helper.colorsMap[record.FieloPRP__Status__c]);
         component.set('v.variant', helper.variantMap[record.FieloPRP__Status__c]);
