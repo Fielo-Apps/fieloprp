e57ae3b2d35fd867e9a5c12d8d6ebdfe396122fe
tibeal <tibeal@gmail.com>
2018-06-21 11:33:24 -0300

    Points bugfixing

--- src/aura/InvoiceStatusCard/InvoiceStatusCardHelper.js
+++ src/aura/InvoiceStatusCard/InvoiceStatusCardHelper.js
@@ -1,12 +1,12 @@
 ({
-	colorsMap: {
+    colorsMap: {
         'Open': 'fielo-invoice-status--pending',
         'Pending for Approval': 'fielo-invoice-status--pending',
         'Approved': 'fielo-invoice-status--approved',
         'Rejected': 'fielo-invoice-status--rejected',
         'Canceled': 'fielo-invoice-status--rejected',
         'New': 'fielo-invoice-status--pending'
-	},
+    },
     iconMap: {
         'Open': 'utility:info',
         'Pending for Approval': 'utility:info',
@@ -14,7 +14,7 @@
         'Rejected': 'utility:warning',
         'Canceled': 'utility:warning',
         'New': 'utility:info'
-	},
+    },
     variantMap: {
         'Open': 'base',
         'Pending for Approval': 'base',
@@ -22,5 +22,15 @@
         'Rejected': 'inverse',
         'Canceled': 'inverse',
         'New': 'base'
+    },
+    format: function() {
+        var num = arguments.length; 
+        var oStr = arguments[0];   
+        for (var i = 1; i < num; i++) { 
+            var pattern = "\\{" + (i-1) + "\\}"; 
+            var re = new RegExp(pattern, "g"); 
+            oStr = oStr.replace(re, arguments[i]); 
+        } 
+        return oStr;
     }
 })
